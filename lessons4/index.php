41268930eb2f4ea6de6a6b35cebfe1ef85c9374e
unknown <baxramovsultonmurod@gmil.>
2025-11-06 16:49:29 +0500

    lesson 4

---
+++ lessons4/index.php
@@ -0,0 +1,97 @@
+<?php
+
+
+$a=[1,3,-9,0,6,87,23,12 ];
+
+$arr=array(
+    array(2,3,5,6,7,8,34,23,2),
+    array()
+    array()
+ 
+
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+    <style>
+      body{
+         background-color: green; 
+      }
+      h1{
+         color:red;
+         border: 10px solid black;
+         border-radius:50px; 
+         text-align:center;
+         padding: 20px;
+         background-color:blue;
+      }
+    </style>
+</head>
+<body>
+   <h1><?=$s?></h1>
+</body>
+</html>
+
+
+
+
+
